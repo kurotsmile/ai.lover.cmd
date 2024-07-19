082f8f614b0e908c03599d95b4636687c0c833dc
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-20 01:39:07 +0700

    Fix list home
    Fix list home

--- js/ai.js
+++ js/ai.js
@@ -3,6 +3,7 @@ class AI{
     onLoad(){
         cr.setColor("#a02bc9");
         cr.loadJs("js/commands.js","cmd","show");
+        cr.add_btn_top();
     }
 
     show_contact(){
@@ -17,14 +18,18 @@ class AI{
 
     box_item(data){
         var empItem=$(`<div class="col-lg-3 col-md-6 mb-4">
-                    <div class="card">
+                    <div class="card animate__flipInX animate__animated">
                         <div class="card-body">
-                            <h5 class="card-title">${data.key}</h5>
+                            <h5 class="card-title"><i class="fas fa-comment"></i> ${data.key}</h5>
                             <p class="card-text">${data.msg}</p>
                         </div>
                     </div>
                 </div>`);
         return empItem;
+    }
+
+    show_setting(){
+        cr.show_setting();
     }
 }
 
--- js/commands.js
+++ js/commands.js
@@ -4,18 +4,37 @@ class AI_Command{
 
     show(){
         $.getJSON(this.url_data_chat,(data)=>{
-            var html='<h1 class="mb-4">AI Lover Command List</h1>';
-            var all_item=data["all_item"];
-
-            html+='<div class="command-section">';
-                html+='<h2>Basic Commands</h2>';
-                html+='<div class="row" id="list_cmd_basic"></div>';
-            html+='</div>';
-            $("#ai_content").html(html);
-            $.each(all_item,function(index,c){
-                $("#list_cmd_basic").append(ai.box_item(c));
-            })
             
+            var html='';
+            html+='<div class="container mt-5">';
+                html+='<h1 class="mb-4">AI Lover Command List';
+                    html+='<button class="btn btn-light btn-sm float-right m-1"><i class="fas fa-list-alt"></i> List</button>';
+                    html+='<button class="btn btn-light btn-sm float-right m-1"><i class="fas fa-th"></i> Grid</button>';
+                html+='</h1>';
+
+                html+='<div class="command-section">';
+                    html+='<h2>Basic Commands</h2>';
+                    html+='<div class="row" id="list_cmd_basic"></div>';
+                html+='</div>';
+
+                html+='<div class="command-section">';
+                    html+='<h2>Advanced Commands</h2>';
+                    html+='<div class="row" id="list_cmd_advanced"></div>';
+                html+='</div>';
+
+            html+='</div>';
+
+            $("#ai_content").html(html);
+
+            var all_item=data["all_item"];
+            $.each(cr.shuffle(cr.limitItem(all_item,50)),function(index,c){
+                $("#list_cmd_basic").append(ai.box_item(c));
+            });
+            
+            $.each(cr.shuffle(cr.limitItem(all_item,50)),function(index,c){
+                $("#list_cmd_advanced").append(ai.box_item(c));
+            });
+
         });
     }
 }
